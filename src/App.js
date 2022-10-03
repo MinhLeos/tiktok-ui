54976323250eb653cccee2f56d94eee7fa8df3c6
Minh Lê <81982128+lequangminh10081990@users.noreply.github.com>
2022-10-03 21:51:02 +0700

    config Router and Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '@/routes';
+import { DefaultLayout } from '@/component/Layouts';
 
 function App() {
    return (
-      <div className="App">
-         <p>Tiktok</p>
-      </div>
+      <Router>
+         <div className="App">
+            <Routes>
+               {publicRoutes.map((route, idx) => {
+                  let Layout = DefaultLayout;
+                  if (route.layout) {
+                     Layout = route.layout;
+                  } else if (route.layout === null) {
+                     Layout = Fragment;
+                  }
+                  const Page = route.component;
+                  return (
+                     <Route
+                        key={idx}
+                        path={route.path}
+                        element={
+                           <Layout>
+                              <Page />
+                           </Layout>
+                        }
+                     />
+                  );
+               })}
+            </Routes>
+         </div>
+      </Router>
    );
 }
 
